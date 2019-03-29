d11ed1e7c7f4ef40ee712642caf01740f5d9ce0b
Rachel R <rrrachelrath@gmail.com>
2019-03-29 19:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   
-  if (string.toUpperCase === uppercase){
+  if (string.toUpperCase === string){
       return 'YES INDEED!'
   }
-  if (string.toLowerCase === lowercase){
+  if (string.toLowerCase === string){
     return `I can't hear you!`
   }
   return 'I love you, too.'

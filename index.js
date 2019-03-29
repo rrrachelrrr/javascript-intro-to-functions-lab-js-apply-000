c2e26b4bbe35da3b4ca2f411deec4c3c062b3519
Rachel R <rrrachelrath@gmail.com>
2019-03-29 19:50:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  
-  if (string.toUpperCase === string){
+  if (string.toUpperCase() === string){
       return 'YES INDEED!'
   }
-  if (string.toLowerCase === string){
+  if (string.toLowerCase() === string){
     return `I can't hear you!`
   }
   else {
